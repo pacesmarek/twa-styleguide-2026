ae72ae4fc6ad4bf9fe1fea4eeffdf98265a73bb6
mpaces <marek.paces@shopup.cz>
2026-03-15 17:58:00 +0100

    @ FIX: Docker Live reload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,12 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://pacesmarek.github.io',
   base: '/twa-styleguide-2026',
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 500
+      },
+    },
+  },
 });
